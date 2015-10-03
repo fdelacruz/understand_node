15002945707bbe9a7fc64a1820f29a0ed46c025f
Francisco De La Cruz <helloquico@gmail.com>
2015-10-03 19:47:42 -0400

    Immediately Invoked Function Expressions (IIFE)

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-// pass by value
-function change(b) {
-	b = 2;
-}
+var firstname = 'Jane';
 
-var a = 1;
-change(a);
-console.log(a);
+(function (lastname) {
+	var firstname = 'John';
+	console.log(firstname);
+	console.log(lastname);
+}('Doe'));
 
-// pass by reference
-function changeObj(d) {
-	d.prop1 = function () {};
-	d.prop2 = {};
-}
-
-var c = {};
-c.prop1 = {};
-changeObj(c);
-console.log(c);
+console.log(firstname);
