0412c75bca34d51e4723f3cfe721f6885db1f089
Francisco De La Cruz <helloquico@gmail.com>
2015-10-05 13:14:49 -0400

    Inherit from the Event Emitter

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var person = {
-	firstname: '',
-	lastname: '',
-	greet: function () {
-		return this.firstname + ' ' + this.lastname;
-	}
+var EventEmitter = require('events');
+var Util = require('util');
+
+function Greetr() {
+	this.greeting = 'Hello World!';
+}
+
+Util.inherits(Greetr, EventEmitter); // Greetr.prototype = EventEmitter.prototype
+
+Greetr.prototype.greet = function (data) {
+	console.log(this.greeting + ': ' + data);
+	this.emit('greet', data);
 };
 
-var john = Object.create(person);
-john.firstname = 'John';
-john.lastname = 'Doe';
+var greeter1 = new Greetr();
 
+greeter1.on('greet', function (data) {
+	console.log('Someone greeted! ' + data);
+});
 
-var jane = Object.create(person);
-jane.firstname = 'Jane';
-jane.lastname = 'Doe';
-
-console.log(john.greet());
-console.log(jane.greet());
+greeter1.greet('Francisco');
