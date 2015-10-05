56f79a4a40460fb357912bbcd05a7fd5ec1905fa
Francisco De La Cruz <helloquico@gmail.com>
2015-10-05 17:40:28 -0400

    .call and .apply

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var name = 'John Doe';
+var obj = {
+	name: 'John Doe',
+	greet: function () {
+		// console.log(`Hello  + ${this.name}`); // Template Literals
+		console.log('Hello ' + this.name);
+	}
+};
 
-var greet = 'Hello ' + name;
-var greet2 = `Hello ${ name }`; // Only in works in the latest version of node
-
-console.log(greet);
-console.log(greet2);
+obj.greet();
+obj.greet.call({ name: 'Jane Doe'}); // 'this' points to a different object
+obj.greet.apply({ name: 'Jane Doe'});
