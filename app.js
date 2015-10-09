c54f75e545565aea25c737e724c8c652599c65a1
Francisco De La Cruz <helloquico@gmail.com>
2015-10-09 06:27:27 -0400

    init, nodemon, and package.json

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-var http = require('http');
-var fs = require('fs');
+var moment = require('moment');
 
-http.createServer(function (req, res) {
-
-	if (req.url === '/') {
-		fs.createReadStream(__dirname + '/index.htm').pipe(res);
-	}
-
-	else if (req.url === '/api') {
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		var obj = {
-			firstname: 'John',
-			lastname: 'Doe'
-		};
-		res.end(JSON.stringify(obj));
-	}
-	else {
-		res.writeHead(404);
-		res.end();
-	}
-
-}).listen(1337, '127.0.0.1');
+console.log(moment().format("ddd, hA"));
