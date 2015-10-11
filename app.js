7e17946b5e801cb5ff70429d5733951f077419f7
Francisco De La Cruz <helloquico@gmail.com>
2015-10-11 19:16:57 -0400

    Querying and Post Parameters

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json()
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -16,8 +20,20 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
+app.post('/person', urlencodedParser, function (req, res) {
+	res.send('Thank You!');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function (req, res) {
+	res.send('Thank you for the JSON data!');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
 app.get('/person/:id', function (req, res) {
-	res.render('person', { ID: req.params.id });
+	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
 });
 
 app.get('/api', function (req, res) {
