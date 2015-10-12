238bb8e32628b85cb87faca853a818923347bcb9
Francisco De La Cruz <helloquico@gmail.com>
2015-10-11 21:28:40 -0400

    RESTful APIs and JSON

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -20,24 +20,27 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
+app.get('/person/:id', function(req, res) {
+	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+});
+
 app.post('/person', urlencodedParser, function (req, res) {
 	res.send('Thank You!');
 	console.log(req.body.firstname);
 	console.log(req.body.lastname);
 });
 
-app.post('/personjson', jsonParser, function (req, res) {
-	res.send('Thank you for the JSON data!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
-
-app.get('/person/:id', function (req, res) {
-	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
-});
-
-app.get('/api', function (req, res) {
+app.get('/api/person/:id', function (req, res) {
+	// get that data for database
 	res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.post('/api/person', jsonParser, function (req, res) {
+	// save to the database
+});
+
+app.delete('/api/person/:id', function (req, res) {
+	// delete from the database
 });
 
 app.listen(port);
