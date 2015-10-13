fda83f37e2a1d89088a2f15d27427e719d74ebbc
Francisco De La Cruz <helloquico@gmail.com>
2015-10-13 12:03:10 -0400

    Mongodb and Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://test:test@ds037234.mongolab.com:37234/addressbook');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+	firstname: String,
+	lastname: String,
+	address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+	firstname: 'John',
+	lastname: 'Doe',
+	addressbook: '555 Main St.'
+});
+
+// save the user
+john.save(function (err) {
+	if (err) throw err;
+
+	console.log('person saved!');
+});
+
+var jane = Person({
+	firstname: 'Jane',
+	lastname: 'Doe',
+	addressbook: '555 Main St.'
+});
+
+// save the user
+jane.save(function (err) {
+	if (err) throw err;
+
+	console.log('person saved!');
+});
+
+var Person = mongoose.model('Person', personSchema);
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -13,8 +54,18 @@ app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url: ' + req.url);
+	
+	// get all the users
+	Person.find({}, function (err, users) {
+		if (err) throw err;
+
+		// object of all the users
+		console.log(users);
+	});
+
 	next();
 });
+
 
 htmlController(app);
 
